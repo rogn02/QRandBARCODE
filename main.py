c25498025333e7692a3e19aa3e99f1f11bd98be3
rogn02 <57052476+rogn02@users.noreply.github.com>
2022-08-25 18:23:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pyqrcode 
+import png 
+from pyqrcode import QRCode 
+import barcode
+from barcode.writer import ImageWriter
+
+def MakeQR(url_input,extension_of_qr,qr_file_name):
+  try:
+    url = pyqrcode.create(url_input)
+    if extension_of_qr=="png":
+      url.png(qr_file_name+'.png', scale = 6)
+    else:
+      url.svg(qr_file_name+'.svg', scale = 8)
+  except:
+    print("There has been an error:\n*Recheck your input\n*Check your module installations")
+
+def MakeBarcode(Input_String,Format):
+  try:
+    barcode_format = barcode.get_barcode_class(Format)
+    The_barcode = barcode_format(Number, writer=ImageWriter())
+    The_barcode.save("generated_barcode")
+  except:
+    print("There has been an error:\n*Recheck your input\n*Check your module installations")
+    
